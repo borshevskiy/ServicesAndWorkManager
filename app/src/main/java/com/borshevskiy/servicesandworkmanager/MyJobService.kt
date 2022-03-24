4aaa50f076646957829f943080ab6f671e355006
SergeBorshevskiy <sk8er1001@mail.ru>
2022-03-24 14:34:40 +0600

    JobService. Создание сервиса

---
+++ app/src/main/java/com/borshevskiy/servicesandworkmanager/MyJobService.kt
@@ -0,0 +1,43 @@
+package com.borshevskiy.servicesandworkmanager
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MyJobService: JobService() {
+
+    private val scope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+        scope.cancel()
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyJobService: $message")
+    }
+
+    override fun onStartJob(p0: JobParameters?): Boolean {
+        log("onStartCommand")
+        scope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer $i")
+            }
+        }
+        jobFinished(p0,true)
+        return true
+    }
+
+    override fun onStopJob(p0: JobParameters?): Boolean {
+        log("onStopJob")
+        return true
+    }
+}
