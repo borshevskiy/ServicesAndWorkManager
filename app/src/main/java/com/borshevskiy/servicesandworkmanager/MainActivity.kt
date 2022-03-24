9cb1dc029677a73f5c75789197742dd0efdbce70
SergeBorshevskiy <sk8er1001@mail.ru>
2022-03-24 12:11:43 +0600

    Foreground Service

--- app/src/main/java/com/borshevskiy/servicesandworkmanager/MainActivity.kt
+++ app/src/main/java/com/borshevskiy/servicesandworkmanager/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.borshevskiy.servicesandworkmanager
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.borshevskiy.servicesandworkmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,25 +19,8 @@ class MainActivity : AppCompatActivity() {
                 startService(MyService.newIntent(this@MainActivity, 25))
             }
             foregroundService.setOnClickListener {
-                showNotification()
+                ContextCompat.startForegroundService(this@MainActivity, MyForegroundService.newIntent(this@MainActivity))
             }
         }
-    }
-
-    private fun showNotification() {
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
-            notificationManager.createNotificationChannel(notificationChannel)
-        }
-        val notification =
-            NotificationCompat.Builder(this, CHANNEL_ID).setContentTitle("Title").setContentText("Text")
-                .setSmallIcon(R.drawable.ic_launcher_background).build()
-        notificationManager.notify(1, notification)
-    }
-
-    companion object {
-        private const val CHANNEL_ID = "channel_id"
-        private const val CHANNEL_NAME = "channel_name"
     }
 }
