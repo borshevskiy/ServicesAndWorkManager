bf9e888da5246e6a860d9c3d62d14e3323ad3ec4
SergeBorshevskiy <sk8er1001@mail.ru>
2022-03-24 10:10:15 +0600

    Services. Создание первого сервиса

--- app/src/main/java/com/borshevskiy/servicesandworkmanager/MainActivity.kt
+++ app/src/main/java/com/borshevskiy/servicesandworkmanager/MainActivity.kt
@@ -2,10 +2,18 @@ package com.borshevskiy.servicesandworkmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.borshevskiy.servicesandworkmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this))
+        }
     }
 }
---
+++ app/src/main/java/com/borshevskiy/servicesandworkmanager/MyService.kt
@@ -0,0 +1,49 @@
+package com.borshevskiy.servicesandworkmanager
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MyService: Service() {
+
+    private val scope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        log("onStartCommand")
+        scope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer $i")
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+        scope.cancel()
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyService: $message")
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, MyService::class.java)
+        }
+    }
+}
